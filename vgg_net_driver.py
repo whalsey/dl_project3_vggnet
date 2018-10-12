80824c8c3e32a2c76e4c72b19c2efa5f1dae53c1
William Halsey <admiral@Williams-MacBook-Pro.local>
2018-10-11 20:26:12 -0400

    m

--- vgg_net_driver.py
+++ vgg_net_driver.py
@@ -35,36 +35,36 @@ import tensorflow as tf
 
 # train vggnet with pretrained weights
 # will create different classes that have the different layers set to not/trainable
-from vggnet_pretrained import vgg16_3
-
-print("initializing network")
-sess = tf.Session()
-net = vgg16_3(weights="vgg16_weights.npz", sess=sess, lr=1e-5, epochs=50, batch=50, decay=0.75)
-
-print("training")
-train_acc, valid_acc, lr_l = net.train(1)
-
-print("testing")
-test_acc = net.test_eval()
-
-with open("output_pretrained3.csv", 'w') as o:
-    buffer = ','.join(["epoch"] + [str(i) for i in range(20)])+'\n'
-    o.write(buffer)
-
-    buffer = ','.join(["training"] + [str(i) for i in train_acc])+'\n'
-    o.write(buffer)
-
-    buffer = ','.join(["validation"] + [str(i) for i in valid_acc])+'\n'
-    o.write(buffer)
-
-    buffer = ','.join(["learning_rate"] + [str(i) for i in lr_l]) + '\n'
-    o.write(buffer)
-
-    buffer = str(test_acc)+'\n'
-    o.write(buffer)
-    o.flush()
-
-sess.close()
+# from vggnet_pretrained import vgg16_3
+#
+# print("initializing network")
+# sess = tf.Session()
+# net = vgg16_3(weights="vgg16_weights.npz", sess=sess, lr=1e-5, epochs=50, batch=50, decay=0.75)
+#
+# print("training")
+# train_acc, valid_acc, lr_l = net.train(1)
+#
+# print("testing")
+# test_acc = net.test_eval()
+#
+# with open("output_pretrained3.csv", 'w') as o:
+#     buffer = ','.join(["epoch"] + [str(i) for i in range(20)])+'\n'
+#     o.write(buffer)
+#
+#     buffer = ','.join(["training"] + [str(i) for i in train_acc])+'\n'
+#     o.write(buffer)
+#
+#     buffer = ','.join(["validation"] + [str(i) for i in valid_acc])+'\n'
+#     o.write(buffer)
+#
+#     buffer = ','.join(["learning_rate"] + [str(i) for i in lr_l]) + '\n'
+#     o.write(buffer)
+#
+#     buffer = str(test_acc)+'\n'
+#     o.write(buffer)
+#     o.flush()
+#
+# sess.close()
 
 from vggnet_pretrained import vgg16_4
 
