1f560b8d44436e2dbcec961480853cda4482969f
William Halsey <admiral@Williams-MacBook-Pro.local>
2018-10-13 10:36:05 -0400

    bug fix

--- fromScratch16.py
+++ fromScratch16.py
@@ -204,7 +204,7 @@ class vgg16:
         # fc2
         with tf.name_scope('fc2') as scope:
             fc2w = tf.Variable(tf.truncated_normal([1024, 1024], dtype=tf.float32, stddev=1e-1), name='weights')
-            fc2b = tf.Variable(tf.constant(1.0, shape=[4096], dtype=tf.float32), trainable=True, name='biases')
+            fc2b = tf.Variable(tf.constant(1.0, shape=[1024], dtype=tf.float32), trainable=True, name='biases')
             fc2l = tf.nn.bias_add(tf.matmul(self.fc1, fc2w), fc2b)
             fc2l = tf.layers.batch_normalization(fc2l, training=self.training)
             self.fc2 = tf.nn.relu(fc2l)
